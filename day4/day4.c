36dca5bf7b044bb06ff93aa86c782568abe9d6ec
Filipe Lemos <filipellemos@gmail.com>
2025-12-02 09:32:07 -0300

    fix: correct everything that was pointed out by bmeneg

--- day4/day4.c
+++ day4/day4.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	FILE *f;
-	int ret = 0;
-	char buffer[1024];
-	char letter[1024][1024];
-	int  i, j, x, y = 0;
-	int sum = 0;
+int main(void)
+{
+		FILE *f;
+		char buffer[1024];
+		char letter[1024][1024];
+		int  i, j, x, y = 0;
+		int sum = 0;
 
-	f = fopen("input", "r");
-	if (!f) {
-		perror("Error opening file");
-		ret = 1;
-		goto exit_0;
-	}
-	while (fgets(buffer, sizeof(buffer), f)) {
-        x = 0;
-        while (buffer[x] != '\n') {
-            letter[y][x] = buffer[x];
-			x++;
-        }
-		y++;	
-	}
+		f = fopen("input", "r");
+		if (!f) {
+				perror("Error opening file");
+				return 1;
+		}
+		while (fgets(buffer, sizeof(buffer), f)) {
+				x = 0;
+				while (buffer[x] != '\n') {
+						letter[y][x] = buffer[x];
+						x++;
+				}
+		y++;
+		}
 	// Part 1
-	for (i = 0; i <= y;  i++){
-			for (j = 0; j <= x; j++){
+		for (i = 0; i <= y;  i++) {
+		for (j = 0; j <= x; j++) {
 				if (letter[i][j] == 'X' && letter[i][j+1] == 'M'
 				&& letter[i][j+2] == 'A' && letter[i][j+3] == 'S')
 						sum++;
 				if (letter[i][j] == 'S' && letter[i][j+1] == 'A'
 				&& letter[i][j+2] == 'M' && letter[i][j+3] == 'X')
-						sum++;						
+						sum++;
 				if (letter[i][j] == 'X' && letter[i+1][j] == 'M'
 				&& letter[i+2][j] == 'A' && letter[i+3][j] == 'S')
 						sum++;
@@ -50,27 +49,26 @@ int main(void) {
 				if (letter[i][j] == 'S' && letter[i+1][j-1] == 'A'
 				&& letter[i+2][j-2] == 'M' && letter[i+3][j-3] == 'X')
 						sum++;
-			}
-	}
-	printf("\nSum of XMAS = %d\n", sum);
-	// Part 2
-	sum = 0;
-	for (i = 0; i <= y;  i++){
-			for (j = 0; j <= x; j++){
-					if ((letter[i][j] == 'M' && letter[i][j+2] == 'S'
-						&& letter[i+1][j+1] == 'A' && letter[i+2][j] == 'M'
-						&& letter[i+2][j+2] == 'S') || (letter[i][j] == 'S'
-						&& letter[i][j+2] == 'M' && letter[i+1][j+1] == 'A'
-						&& letter[i+2][j] == 'S' && letter[i+2][j+2] == 'M')
-						|| (letter[i][j] == 'S' && letter[i][j+2] == 'S'
-						&& letter[i+1][j+1] == 'A' && letter[i+2][j] == 'M'
-						&& letter[i+2][j+2] == 'M') || (letter[i][j] == 'M'
-						&& letter[i][j+2] == 'M' && letter[i+1][j+1] == 'A'
-						&& letter[i+2][j] == 'S' && letter[i+2][j+2] == 'S'))
+		}
+		}
+		printf("\nSum of XMAS = %d\n", sum);
+		// Part 2
+		sum = 0;
+		for (i = 0; i <= y;  i++) {
+		for (j = 0; j <= x; j++) {
+				if ((letter[i][j] == 'M' && letter[i][j+2] == 'S'
+				&& letter[i+1][j+1] == 'A' && letter[i+2][j] == 'M'
+				&& letter[i+2][j+2] == 'S') || (letter[i][j] == 'S'
+				&& letter[i][j+2] == 'M' && letter[i+1][j+1] == 'A'
+				&& letter[i+2][j] == 'S' && letter[i+2][j+2] == 'M')
+				|| (letter[i][j] == 'S' && letter[i][j+2] == 'S'
+				&& letter[i+1][j+1] == 'A' && letter[i+2][j] == 'M'
+				&& letter[i+2][j+2] == 'M') || (letter[i][j] == 'M'
+				&& letter[i][j+2] == 'M' && letter[i+1][j+1] == 'A'
+				&& letter[i+2][j] == 'S' && letter[i+2][j+2] == 'S'))
 						sum++;
-			}
-	}
-	printf("\nSum of XMAS Part 2 = %d\n", sum);
-	exit_0:
-		return ret;
+		}
+		}
+		printf("\nSum of XMAS Part 2 = %d\n", sum);
+		return 0;
 }
