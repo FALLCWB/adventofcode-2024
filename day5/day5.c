f231b559b83f34c9e331e79b518b65716e2c2492
Filipe Lemos <filipellemos@gmail.com>
2025-12-03 22:05:33 -0300

    fix: made many improvements to the code based on suggestions provided by bmeneg

--- day5/day5.c
+++ day5/day5.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define YES 1
-#define NO 0
+#include <stdbool.h>
 
 struct rule {
 	int value;
 	struct rule *next;
 };
+
 void insert (struct rule *list[], int idx, int value)
 {
 	struct rule *node = malloc(sizeof(struct rule));
@@ -18,6 +17,7 @@ void insert (struct rule *list[], int idx, int value)
 	node->next = list[idx];
 	list[idx] = node;
 }
+
 void clear_list(struct rule *list[])
 {
 	int idx;
@@ -31,18 +31,16 @@ void clear_list(struct rule *list[])
 		list[idx] = NULL;
 	}
 }
+
 int main(void)
 {
 	FILE *f;
-	struct rule *list[100], *print;
-	int i, j, pages[25], size, idx, value, correct, current, cp;
-	int middles = 0;
-	int mcorrected = 0;
+	struct rule *list[100] = { 0 };
+	struct rule *print;
+	int i, j, pages[25], size, idx, value, current, cp, middles, mcorrected;
+	bool correct;
 	char line[256], *token;
 
-	for (i = 0; i < 100; i++) {
-		list[i] = NULL;
-	}
 	f = fopen("input", "r");
 	if (!f) {
 		perror("Error opening file");
@@ -51,18 +49,19 @@ int main(void)
 	while (fgets(line, sizeof(line), f)) {
 		if (line[0] == '\n' || line[0] == '\0')
 			continue;
+
 		if (strchr(line, '|')) {
 			if (sscanf(line, "%d|%d", &idx, &value) == 2)
 				insert(list, idx, value);
-		}
-		else if (strchr(line, ',')) {
+		} else if (strchr(line, ',')) {
 			size = 0;
 			token = strtok(line, ",\n");
 			while (token){
 				pages[size++] = atoi(token);
 				token = strtok(NULL, ",\n");
 			}
-			correct = YES;
+
+			correct = true;
 			for (i = 0; i < size; i++){
 				current = pages[i];
 				cp = i;
@@ -70,7 +69,7 @@ int main(void)
 					print = list[current];
 					while (print != NULL){
 						if (print->value == pages[j]) {
-							correct = NO;
+							correct = false;
 							pages[cp] = pages[j];
 							pages[j] = current;
 							cp = j;
@@ -79,10 +78,9 @@ int main(void)
 					}
 				}
 			}
-			if (correct == YES) {
+			if (correct) {
 				middles += pages[size / 2];
-			}
-			else {
+			} else {
 				mcorrected += pages[size / 2];
 			}
 		}
